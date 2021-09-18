2c97be1840679ef0b68c726621b8ad03f5661ce7
Esko Luontola <esko@luontola.fi>
2021-09-18 20:23:22 +0300

    Write initialized-classes.txt

--- src/main/java/fi/luontola/clojure_native_image_agent/Agent.java
+++ src/main/java/fi/luontola/clojure_native_image_agent/Agent.java
@@ -1,20 +1,38 @@
 package fi.luontola.clojure_native_image_agent;
 
+import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.ProtectionDomain;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Agent {
 
-    public static void premain(String agentArgs, Instrumentation inst) throws ClassNotFoundException {
+    public static void premain(String agentArgs, Instrumentation inst) throws ClassNotFoundException, IOException {
         Map<String, String> args = parseArgs(agentArgs);
-        String mainClass = requiredArg(args, "main-class");
+        String initializeClass = requiredArg(args, "initialize-class");
+        Path outputDir = Paths.get(requiredArg(args, "output-dir"));
 
+        List<String> classes = trackRecursivelyLoadedClasses(initializeClass, inst);
+        classes.sort(null);
+
+        Files.createDirectories(outputDir);
+        Path file = outputDir.resolve("initialized-classes.txt");
+        Files.write(file, classes);
+        log("Initialized " + initializeClass + " and it loaded " + classes.size() + " classes, " +
+                "which are now listed in " + file);
+    }
+
+    private static List<String> trackRecursivelyLoadedClasses(String className, Instrumentation inst) throws ClassNotFoundException {
         Queue<String> loadedClasses = new ConcurrentLinkedQueue<>();
         ClassFileTransformer transformer = new ClassFileTransformer() {
             @Override
@@ -27,11 +45,9 @@ public class Agent {
             }
         };
         inst.addTransformer(transformer);
-        Class.forName(mainClass);
+        Class.forName(className);
         inst.removeTransformer(transformer);
-
-        System.out.println("loadedClasses.size() = " + loadedClasses.size());
-        System.out.println("loadedClasses = " + loadedClasses);
+        return new ArrayList<>(loadedClasses);
     }
 
     public static Map<String, String> parseArgs(String args) {
@@ -51,5 +67,9 @@ public class Agent {
             throw new IllegalArgumentException("Missing required argument: " + key);
         }
         return value;
+    }
+
+    private static void log(String message) {
+        System.out.println("clojure-native-image-agent: " + message);
     }
 }
